344e837836a9a92bcf40706f06d4657933769dc0
frocket-exe <freddie.rockett@gmail.com>
2026-08-12 15:51:38 +0100

    Started board states json usage

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import chess
+import json
 
 board = chess.Board()
 
@@ -13,12 +14,27 @@ def tryMove(board, oldSquareName, newSquareName):
     except:
         print("Move not legal!")
 
-print(board)
-tryMove(board, "e2", "e5")
-tryMove(board, "e2", "e3")
-tryMove(board, "g1", "f3")
-tryMove(board, "g1", "g3")
-tryMove(board, "e2", "e4")
-tryMove(board, "e7", "e5")
-tryMove(board, "d2", "d4")
-tryMove(board, "e5", "d4")
+# print(board)
+# tryMove(board, "e2", "e5")
+# tryMove(board, "e2", "e3")
+# tryMove(board, "g1", "f3")
+# tryMove(board, "g1", "g3")
+# tryMove(board, "e2", "e4")
+# tryMove(board, "e7", "e5")
+# tryMove(board, "d2", "d4")
+# tryMove(board, "e5", "d4")
+
+def boardChange(oldBoard, newBoard):
+    global movingPiece
+    oldEmpty = oldBoard.count(0)
+    # oldFull = oldBoard.count(1)
+    newEmpty = newBoard.count(0)
+    # newFull = newBoard.count(1)
+    if newEmpty > oldEmpty:
+        movingPiece = 
+
+with open ("board-states.json") as f:
+    boardStates = json.load(f)
+
+movingPiece = ""
+boardChange(boardStates[0], boardStates[1])
