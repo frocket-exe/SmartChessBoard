64ac15f55eedd9bfde0c75597fa4d7353186564d
frocket-exe <freddie.rockett@gmail.com>
2026-08-13 14:03:07 +0100

    Validate not moving piece and piece colour is correct

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import json
 board = chess.Board()
 
 def tryMove(board, oldSquareName, newSquareName):
-    oldSquare = (chess.parse_square(oldSquareName))
-    newSquare = (chess.parse_square(newSquareName))
+    # oldSquare = (chess.parse_square(oldSquareName))
+    # newSquare = (chess.parse_square(newSquareName))
     print()
     try:
-        moveAttempt = board.find_move(oldSquare, newSquare)
+        moveAttempt = board.find_move(oldSquareName, newSquareName)
         board.push(moveAttempt)
         print(board)
     except:
@@ -24,17 +24,38 @@ def tryMove(board, oldSquareName, newSquareName):
 # tryMove(board, "d2", "d4")
 # tryMove(board, "e5", "d4")
 
-def boardChange(oldBoard, newBoard):
-    global movingPiece
+def indexToBoardNum(arrIndex):
+    row = 7-(arrIndex//8)
+    column = arrIndex%8
+    boardNum = row*8 + column
+    return boardNum
+
+def findMoved(oldBoard, newBoard):
+    for i in range(0, 63):
+        if(oldBoard[i] != newBoard[i]):
+            return (indexToBoardNum(i))
+
+
+def boardChange(board, oldBoard, newBoard):
+    global moveFrom
     oldEmpty = oldBoard.count(0)
     # oldFull = oldBoard.count(1)
     newEmpty = newBoard.count(0)
     # newFull = newBoard.count(1)
     if newEmpty > oldEmpty:
-        movingPiece = 
+        tryMoveFrom = findMoved(oldBoard, newBoard)
+        if board.turn == board.piece_at(tryMoveFrom).color:
+            moveFrom = tryMoveFrom
+    if newEmpty < oldEmpty:
+        moveTo = findMoved(oldBoard, newBoard)
+        if moveTo != moveFrom:
+            tryMove(board, moveFrom, moveTo)
+
 
 with open ("board-states.json") as f:
     boardStates = json.load(f)
 
-movingPiece = ""
-boardChange(boardStates[0], boardStates[1])
+moveFrom = ""
+print(board)
+for i in range(1, len(boardStates)):
+    boardChange(board, boardStates[i-1], boardStates[i])
